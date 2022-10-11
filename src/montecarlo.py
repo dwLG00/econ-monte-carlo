89a10c5b2a6ac6f5f3451214682f59cf4ecb2e52
dwLG00 <wallace.dylan00@gmail.com>
2022-10-11 00:12:50 -0700

    Added monte carlo Y function

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -1,2 +1,15 @@
 import numpy as np
 import random
+
+# MESSAGE TO JACK: I'm unsure about the math for all of this, so you should double check
+
+# dlog(y) = dY/Y ~ Norm(0, sigma^2 dt)
+# dlog(x) = dX/X ~ Norm(mu dt, Y^2 dt)
+
+# We can integrate dY = sigma Y dB using Ito's lemma to get Y = Y0 * e^(sigma t - sigma^2 / 2 B)
+# Then, because we know that dlog(x) ~ Norm(mu dt, Y^2 dt), and because dlog(x) = dX/X, we can say that X = X0 * e^dlog(x)
+
+def Y(B, t, Y0, sigma):
+	return Y0 * math.e**(sigma * t - sigma**2 / 2 * B)
+
+#TODO: Equation for X
